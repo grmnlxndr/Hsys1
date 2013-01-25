f96f7b81cf345d527234aa2e6dda92629b2e3e37
QuinoNT <QuinoNT@QuinoNT-PC>
2013-01-25 09:18:31 -0300

    Repositorio Donacion

--- Hsys1/src/HSYS/MainBundle/Entity/DonacionRepository.php
+++ Hsys1/src/HSYS/MainBundle/Entity/DonacionRepository.php
@@ -12,4 +12,59 @@ use Doctrine\ORM\EntityRepository;
  */
 class DonacionRepository extends EntityRepository
 {
+    
+    public function findDonante($criterio, $busqueda, $hasta = NULL){
+        $resultado= null;
+        //consulta por el id del donante
+        if ($criterio == "donante"){
+            $resultado = $this->findDonacionPorDonante($busqueda);
+        };
+        //consulta por el id de la donacion
+        if ($criterio == "id") {
+            $resultado = $this->findDonacionPorPorId($busqueda);
+        };
+        //rango de fecha, busqueda es desde.
+        if ($criterio == "fecha") {
+            $resultado = $this->findDonacionPorRangoDeFecha($busqueda, $hasta);
+        };
+        
+        return $resultado;
+}
+    
+    public function findDonacionPorDonante($donante) {
+        $em = $this->getEntityManager();
+        $dql = "select d from HSYSMainBundle:Donacion d where d.donante like :donante";
+
+        $query = $em->createQuery($dql);
+        $query->setParameter('donante', '%'.$donante.'%');
+        
+        $donaciones = $query->getResult();
+        return $donaciones;
+    }
+    
+    public function findDonacionPorPorId($id) {
+        $em = $this->getEntityManager();
+        $dql = "select d from HSYSMainBundle:Donacion d where d.id like :id";
+
+        $query = $em->createQuery($dql);
+        $query->setParameter('id', '%'.$id.'%');
+        
+        $donaciones = $query->getResult();
+        return $donaciones;
+    }
+    //UPPER(u.name)
+        public function findDonacionPorRangoDeFecha($desde, $hasta) {
+        $em = $this->getEntityManager();
+        $dql = "select d from HSYSMainBundle:Donacion d where d.fechextraccion => :desde and d.fechextraccion =< :hasta";
+
+        $query = $em->createQuery($dql);
+        $query->setParameter('desde', $desde);
+        $query->setParameter('hasta', $hasta);
+        
+        $donaciones = $query->getResult();
+        return $donaciones;
+    }
+    
+    
+    
 }
