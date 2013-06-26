8c458df43e60e5f741553a3e0e85b0a5303d496f
German <German@German-DELL>
2013-06-26 19:54:38 -0300

    Informes diarios analisis.

--- Hsys1/src/HSYS/MainBundle/Controller/InformesController.php
+++ Hsys1/src/HSYS/MainBundle/Controller/InformesController.php
@@ -8,4 +8,15 @@ class InformesController extends Controller
     public function indexAction(){
         return $this->render('HSYSMainBundle:Informes:index.html.twig');
     }
+    
+    public function informeanalisisAction(){
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST'){
+            $em = $this->getDoctrine()->getEntityManager();
+            $fecha = $request->request->get('fecha');
+            $analisis = $em->getRepository('HSYSMainBundle:Analisis')->findAnalisisPorFecha($fecha);
+            return $this->render('HSYSMainBundle:Informes:informeanalisisimprimir.html.twig', array('analisis'=>$analisis, 'fecha'=>$fecha,));
+        }
+        return $this->render('HSYSMainBundle:Informes:informeanalisis.html.twig');
+    }
 }
