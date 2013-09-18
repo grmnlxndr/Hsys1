c7fed39ad755ff925562cc3b31144591f47c3e60
German <German@German-U>
2013-09-18 15:31:10 -0300

    Seguridad para no cambiar receptor. Cambiado algo del twig para que se vea lindo

--- Hsys1/src/HSYS/MainBundle/Controller/DonacionController.php
+++ Hsys1/src/HSYS/MainBundle/Controller/DonacionController.php
@@ -589,6 +589,9 @@ class DonacionController extends Controller {
     public function asignarReceptorFinalAction($don,$rec){
         $em = $this->getDoctrine()->getEntityManager();
         $donacion = $em->getRepository('HSYSMainBundle:Donacion')->find($don);
+        if ($donacion->getReceptor()->getId() != 1){
+            return $this->render('HSYSMainBundle:Donacion:error.html.twig', array('razon' => 'ACCESO DENEGADO: No se puede asignar receptor'));
+        }
         $receptor = $em->getRepository('HSYSMainBundle:Donante')->find($rec);
         $donacion->setReceptor($receptor);
         $em->persist($donacion);
